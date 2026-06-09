9ca3faaf1eefff97ca3aa6c316fe79af459571e3
lucas fraga <152925470+doidaolucao@users.noreply.github.com>
2026-06-08 22:10:13 -0300

    Inicializado o springboot do projeto 🍃

---
+++ src/main/java/lucasdev/sistema_de_chamados/SistemaDeChamadosApplication.java
@@ -0,0 +1,13 @@
+package lucasdev.sistema_de_chamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeChamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeChamadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucasdev/sistema_de_chamados/SistemaDeChamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package lucasdev.sistema_de_chamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeChamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
